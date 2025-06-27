81bc6d9c1adfc8b95dc67e719890ba761126ce59
TheHardest18 <isias1626@gmail.com>
2025-06-27 12:49:02 -0400

    [UPD] __manifest__ creator_firms

--- base/creator_firms/__manifest__.py
+++ base/creator_firms/__manifest__.py
@@ -8,7 +8,7 @@
     """,
     'author': "isias1626@gmail.com",
     'category': 'Application',
-    'version': '15.0.1.0.1',
+    'version': '17.0.1.0.1',
     'depends': ['base', 'report_qweb_element_page_visibility'],
     'data': [
         'security/ir.model.access.csv',
